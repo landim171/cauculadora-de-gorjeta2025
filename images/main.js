850b802b1ad6b28bc12a8796b96365f2e0b989d6
landim171 <daniel.landim@eaportal.org>
2025-04-09 20:00:44 -0300

    inicio da logica da porcentagem da gorjeja

--- images/main.js
+++ images/main.js
@@ -1,6 +1,7 @@
 let bill = 0
 let tipPercentage = 0
 let numberOfPeople = 0
+let buttonSelected = null
 
 function receiveBillValue() {
     bill = document.querySelector("#bill").ValueAsNumber
@@ -13,3 +14,10 @@ function receiveNumberOfPeopleValue() {
 }
 
 console.log(numberOfPeople)
+
+function receiveTipPorcentageValue(value) {
+    tipPercentage = value / 100
+    
+    buttonSelected = document.querySelector('#button-${value}')
+    buttonSelected.classList.add("button-selected")
+}
