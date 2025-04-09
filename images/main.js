c9102e8923d772b0799a051062b426b9d608f023
landim171 <daniel.landim@eaportal.org>
2025-04-09 19:41:03 -0300

    primeira aplicação do java

---
+++ images/main.js
@@ -0,0 +1,15 @@
+let bill = 0
+let tipPercentage = 0
+let numberOfPeople = 0
+
+function receiveBillValue() {
+    bill = document.querySelector("#bill").ValueAsNumber
+}
+
+console.log(bill)
+
+function receiveNumberOfPeopleValue() {
+    numberOfPeople = document.querySelector("#people").valueAsNumber
+}
+
+console.log(numberOfPeople)
