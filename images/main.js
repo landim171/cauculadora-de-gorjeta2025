beaa2bd21ba7525582ce4b1616dda29ef8a55c5d
landim171 <daniel.landim@eaportal.org>
2025-04-09 21:06:29 -0300

    criação da função para criar resultados

--- images/main.js
+++ images/main.js
@@ -5,12 +5,17 @@ let buttonSelected = null
 
 function receiveBillValue() {
     bill = document.querySelector("#bill").ValueAsNumber
+
+    calculateResult()
 }
 
 console.log(bill)
 
+
 function receiveNumberOfPeopleValue() {
     numberOfPeople = document.querySelector("#people").valueAsNumber
+
+    calculateResult()
 }
 
 console.log(numberOfPeople)
@@ -24,10 +29,14 @@ function receiveTipPercentageValue(value) {
 
     buttonSelected = document.querySelector('#button-${value}')
     buttonSelected.classList.add("button-selected")
+
+    calculateResult()
 }
 
- function reciveNumberOfPeopleValue(){
+ function reciveNumberOfPeopleValue() {
     tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
+
+
  }
 
 
@@ -36,4 +45,27 @@ function removeClassButtonSelected() {
         buttonSelected.classList.remove("button-selected")
         buttonSelected = null
     }
+}
+
+function calculateResult() {
+    if (bill !== 0 && tipPercentage !== && numberOfPeople !== 0) {
+        let tipAmoutPersoncalculateTipAmoutPerson()
+        calculateTotalPerson(tipAmoutPerson)
+
+        
+    }
+}
+
+function calculateTipAmoutPerson() {
+    let tipAmoutStrong = document.querySelector(".amout strong")
+    let totalAmoutPerson = bill / tipPercentage + numberOfPeople
+    tipAmoutStrong.textContent = '$${totalAmoutPerson.toFixed(2)}'
+    return tipAmoutStrong
+}
+
+function calculateTotalPerson(tipAmoutPerson) {
+    let totalStrong = document.querySelector(".total strong")
+        let totalAmoutPerson = bill / numberOfPeople + tipAmoutPerson
+        totalStrong.textContent = '$${totalAmoutPerson.toFixed(2)}'
+
 }
